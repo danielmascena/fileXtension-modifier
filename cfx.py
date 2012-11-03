39f511f8d8e4207689546b2ac160ff2e1f138b66
DanielMascena <danielmascena@gmail.com>
2012-11-03 21:33:11 -0200

    Added support for the images directory path

--- cfx.py
+++ cfx.py
@@ -1,9 +1,9 @@
 import os,glob,sys
 
-os.chdir(os.curdir)
 try:
 	current = sys.argv[1]
 	new = sys.argv[2]
+	os.chdir(sys.argv[3] if len(sys.argv) == 4 else os.curdir)
 	for fi in glob.glob("*." + current):
    		os.rename(fi, fi[:-3] + new)
 except IndexError:
